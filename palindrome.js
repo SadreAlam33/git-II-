42ee81e2735073b3e739a87fcff021c4291f70a9
SadreAlam33 <sadrealam33@gmail.com>
2022-12-08 20:19:17 +0530

    check code

---
+++ palindrome.js
@@ -0,0 +1,10 @@
+let str="racecar";
+let bag=""
+for(let i=str.length; i>=; i--){
+	bag += str[i];
+}
+	if(bag==str){
+  	console.log("Palindrome")
+	}else{
+		console.log("Not Palindrome")
+	}
---
+++ prime1.js
@@ -0,0 +1,19 @@
+function checkprime(num) {
+  let count=0;
+	for(let i=0; i<=num; i++){
+		if(num%i ==0){
+			count++
+		}
+	}
+	if(count ==2){
+		return true;
+	}else{
+		return false;
+	}
+}
+let ans=(checkprime(13));
+if(ans==true){
+  console.log("Prime");
+}else{
+  console.log("Not Prime");
+}
